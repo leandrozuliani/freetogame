89080587eb5181eb363a0ffb8a0ddbd480f29f2c
Leandro Zuliani <lfzuliani@gmail.com>
2024-08-04 04:56:49 -0300

    add cache in github action steps

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   testWidgets('test initial MyApp render', (WidgetTester tester) async {
