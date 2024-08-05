dc04578c1520ef6e15237c2f69c3c646a7b70fe0
leandrozuliani <leandrozuliani@users.noreply.github.com>
2024-08-05 05:07:35 +0000

    deploy: c01fc62e862101cbcd128b22a17586988cd73959

--- android/app/src/main/kotlin/com/example/freetogame/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.freetogame
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- test/widget_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('test initial MyApp render', (WidgetTester tester) async {
-    expect(1, 1);
-  });
-}
