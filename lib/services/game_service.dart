741d39d3aac56b6ee1d7529a04ee5e7eaf0fd3a0
Leandro Zuliani <lfzuliani@gmail.com>
2024-08-06 02:45:26 -0300

    including headers api

--- lib/services/game_service.dart
+++ lib/services/game_service.dart
@@ -1,14 +1,20 @@
 import 'dart:convert';
 import 'package:freetogame/models/game_details.dart';
 import 'package:http/http.dart' as http;
-import '../models/game.dart';
+import 'package:freetogame/models/game.dart';
 
 class GameService {
   static const String baseUrl =
       'https://free-to-play-games-database.p.rapidapi.com';
 
+  static const Map<String, String> headers = {
+    'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com',
+    'x-rapidapi-key': String.fromEnvironment('X_RAPIDAPI_KEY', defaultValue: ''),
+  };
+
   static Future<List<Game>> fetchGames() async {
     final request = http.Request('GET', Uri.parse('$baseUrl/api/games'));
+    request.headers.addAll(headers);
 
     final response = await request.send();
     final jsonList = jsonDecode(await response.stream.bytesToString());
@@ -17,9 +23,10 @@ class GameService {
   }
 
   static Future<GameDetails> fetchSpecificGame(int id) async {
-    String gameDetailUrl = '$baseUrl/api/game?id=$id';
+    final gameDetailUrl = '$baseUrl/api/game?id=$id';
 
     final request = http.Request('GET', Uri.parse(gameDetailUrl));
+    request.headers.addAll(headers);
 
     final response = await request.send();
     final jsonMap = jsonDecode(await response.stream.bytesToString());
